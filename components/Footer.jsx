960ffda807f60cb66d0749c49d76dbe289b35109
EdSabhin <edsabhin@yahoo.com>
2024-04-16 10:48:13 -0500

    Footer information added.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,51 +1,51 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="footer items-center p-4 pt-10 bg-neutral text-neutral-content">
-      <div className="items-center grid-flow-col">
-        <p>{`Copyright © ${currentYear} - All rights reserved`}</p>
-      </div>
-      <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="fill-current"
-          >
-            <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-          </svg>
-        </a>
-        <a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="fill-current"
-          >
-            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-          </svg>
-        </a>
-        <a>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            className="fill-current"
-          >
-            <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-          </svg>
-        </a>
-      </div>
-      <div>
+    <footer className="footer justify-center items-center pt-8 pb-10 bg-neutral text-neutral-content text-md text-indigo-to-teal">
+      <div className="flex items-center gap-20">
+        <p>
+          <span className="uppercase">
+            Fialta Currency Conversion -
+          </span>{" "}
+          {`Copyright © ${currentYear} - All rights reserved`}
+        </p>
         <a
-          href="https://www.flaticon.com/free-icons/left-and-right-arrows"
-          title="left and right arrows icons"
+          href=""
+          target="_blank"
+          className="hover:text-teal-200 hover:underline underline-offset-2 cursor-pointer"
         >
-          Left and right arrows icons created by Laisa Islam Ani - Flaticon
+          A project by Ed Sabhin
+        </a>
+        <a
+          href="https://github.com/EdSabhin/fialta-currency-conversion"
+          target="_blank"
+          className="flex items-center gap-4 hover:text-teal-200 cursor- group"
+        >
+          <p className="group-hover:underline underline-offset-2 cursor-pointer">
+            Code Repository
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+          </svg>
+        </a>
+        <a
+          href="https://www.frankfurter.app/"
+          target="_blank"
+          className="hover:text-teal-200 hover:underline underline-offset-2 cursor-pointer"
+        >
+          Powered by Frankfurter.app
         </a>
       </div>
     </footer>
