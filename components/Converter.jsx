f513fd83a0372d0ae6fd31788be4333500b33106
EdSabhin <edsabhin@yahoo.com>
2024-04-17 11:00:55 -0500

    Added error validation message to Converter. Fixed functional bugs.

--- components/Converter.jsx
+++ components/Converter.jsx
@@ -10,17 +10,21 @@ import SwitchButton from "./SwitchButton";
 
 const Converter = ({ currencies }) => {
   const [conversion, setConversion] = useState("");
-
   const [selectFrom, setSelectFrom] = useState("");
   const [selectTo, setSelectTo] = useState("");
   const [amount, setAmount] = useState("");
   const [currencyUnit, setCurrencyUnit] = useState("");
+  const [inputsNotValid, setInputsNotValid] = useState(false);
 
   const handleConversion = () => {
-    if (amount !== "") {
-      conversionData(amount, selectFrom, selectTo).then(setConversion);
-      conversionData(1, selectFrom, selectTo).then(setCurrencyUnit);
+    if (selectFrom === "" || selectTo === "" || amount === "") {
+      setInputsNotValid(true)
     }
+      if (selectFrom !== "" && selectTo !== "" && amount !== "") {
+        conversionData(amount, selectFrom, selectTo).then(setConversion);
+        conversionData(1, selectFrom, selectTo).then(setCurrencyUnit);
+        setInputsNotValid(false)
+      } 
   };
 
   useEffect(() => {
@@ -42,7 +46,7 @@ const Converter = ({ currencies }) => {
   console.log(selectTo);
 
   return (
-    <div className="w-[50%] flex flex-col j items-start gap-24 pb-14">
+    <div className="w-[50%] flex flex-col items-start gap-24 pb-14">
       <div className="flex flex-col gap-16">
         <div className="flex items-end gap-10">
           <Select
@@ -63,9 +67,20 @@ const Converter = ({ currencies }) => {
           <CurrencyInput amount={setAmount} />
           <ConversionButton conversionData={handleConversion} />
         </div>
+        {inputsNotValid && (
+          <div className="w-[96%] py-2 rounded-md bg-rose-100 text-rose-500 text-center">
+            Please select both currencies and an amount to convert.
+          </div>
+        )}
       </div>
       {conversion && currencyUnit && (
-        <div className="flex flex-col  gap-4 items-start pt-7 pl-7 border-t-2 border-l-2 border-slate-200 rounded-tl-md">
+        <div className="relative flex flex-col items-start gap-4 pt-7 pl-7 border-t-2 border-l-2 border-slate-200 rounded-tl-md">
+          <div
+            onClick={() => setConversion("")}
+            className="absolute top-1 right-1 text-2xl text-slate-400 hover:text-blue-500 hover:animate-ping cursor-pointer transition duration-500"
+          >
+            x
+          </div>
           <h2 className="text-xl">
             Today&apos;s Conversion of{" "}
             <span className="font-semibold text-blue-500">{selectFrom}</span> to
