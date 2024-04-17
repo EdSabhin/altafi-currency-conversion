92806d377e8130999a7a2a0aea2972620e1e630b
EdSabhin <edsabhin@yahoo.com>
2024-04-17 09:50:25 -0500

    Dashboard corrections.

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,10 +1,10 @@
 const Dashboard = ({ currencies }) => {
   return (
     <div className="w-[19.3%] h-64 overflow-y-auto rounded-bl-md !rounded-br-none">
-      <h2 className="flex justify-center py-2 z-10 rounded-tl-md bg-[#021431] text-indigo-to-teal">
-        Currency Code Reference
+      <h2 className="flex justify-center px-2 py-2 z-10 rounded-tl-md bg-[#021431] text-indigo-to-teal">
+        <span className="pr-2">*</span> Currency Code Reference <span className="pl-2">*</span>
       </h2>
-      <table className="w-full table table-compact !rounded-br-none">
+      <table className="w-full table table-compact !rounded-br-none text-center">
         <thead>
           <tr className="sticky top-0">
             <th className="text-[0.9rem] pt-3 !rounded-none">Code</th>
