603a779a1918abdd70f424ee711bd558311674fa
EdSabhin <edsabhin@yahoo.com>
2024-04-18 11:58:24 -0500

    Dashboard open/closed state.

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,25 +1,39 @@
+"use client";
+
+import { useState } from "react";
+
 const Dashboard = ({ currencies }) => {
+  const [viewDashboard, setViewDashboard] = useState(true);
+
   return (
-    <div className="h-64 overflow-y-auto rounded-bl-md !rounded-br-none">
-      <h2 className="flex justify-center px-2 py-2 z-10 rounded-tl-md bg-[#021431] text-indigo-to-teal">
-        <span className="pr-2">*</span> Currency Code Reference <span className="pl-2">*</span>
+    <div className="w-[20%] h-64 overflow-y-auto rounded-bl-md !rounded-br-none">
+      <h2
+        onClick={() => setViewDashboard(!viewDashboard)}
+        className={`flex justify-center px-2 py-2 z-10 rounded-tl-md ${
+          !viewDashboard && "`w-[35%] rounded-md"
+        } bg-[#021431] text-indigo-to-teal cursor-pointer`}
+      >
+        <span className="pr-2">*</span> Currency Code Reference{" "}
+        <span className="pl-2">*</span>
       </h2>
-      <table className="w-full table table-compact !rounded-br-none text-center">
-        <thead>
-          <tr className="sticky top-0">
-            <th className="text-[0.9rem] pt-3 !rounded-none">Code</th>
-            <th className="text-[0.9rem] pt-3 !rounded-none">Name</th>
-          </tr>
-        </thead>
-        <tbody className="!rounded-br-none">
-          {Object.entries(currencies).map(([code, name]) => (
-            <tr key={code}>
-              <td className="py-3 !rounded-br-none">{code}</td>
-              <td className="!rounded-br-none">{name}</td>
+      {viewDashboard && (
+        <table className="w-full table table-compact !rounded-br-none">
+          <thead>
+            <tr className="sticky top-0">
+              <th className="text-[0.9rem] pt-3 !rounded-none">Code</th>
+              <th className="text-[0.9rem] pt-3 !rounded-none">Name</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="!rounded-br-none">
+            {Object.entries(currencies).map(([code, name]) => (
+              <tr key={code}>
+                <td className="py-3 !rounded-br-none">{code}</td>
+                <td className="!rounded-br-none">{name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
