b1d77389b3e1d684ee0438e43c4eb9f986d8c679
EdSabhin <edsabhin@yahoo.com>
2024-04-17 19:01:17 -0500

    New Fialta Logo.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,35 +10,17 @@ const orbitron = Orbitron({
 const Nav = () => {
   return (
     <div
-      className={`navbar ${orbitron.className} w-full flex justify-center items-center py-6  gap-12 bg-neutral text-neutral-content`}
+      className={`navbar ${orbitron.className} w-full flex justify-center items-center py-6 gap-8 bg-neutral text-neutral-content`}
     >
-      <div className="flex gap-4">
-        <Image
-          src="/AI-fialta-logo.png"
-          alt=""
-          width={50}
-          height={50}
-          className="rounded-full"
-        />
-        <a className="font-semibold normal-case text-3xl">FIALTA</a>
-      </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
-      </div>
+      <Image
+        src="/fialta-logo.svg"
+        alt="Fialta Logo"
+        width={80}
+        height={80}
+      />
+      <a className="font-semibold normal-case text-3xl tracking-widest">
+        Fialta
+      </a>      
     </div>
   );
 };
