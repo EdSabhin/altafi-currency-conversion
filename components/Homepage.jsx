ff7bc2f38566c40dcf31f7c47bf48b4be9f1ce11
EdSabhin <edsabhin@yahoo.com>
2024-04-25 10:44:18 -0500

    LoadingCurrencies component.

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 import { currencyData } from "@service/currencyData";
+
+import LoadingCurrencies from "./LoadingCurrencies";
 import Converter from "./Converter";
 import Dashboard from "./Dashboard";
 import CurrencyChart from "./CurrencyChart";
@@ -9,26 +11,32 @@ import TopFive from "./TopFive";
 
 const Homepage = () => {
   const [currencies, setCurrencies] = useState("");
+  const [loadingCurrencies, setLoadingCurrencies] = useState(true);
 
   useEffect(() => {
-    currencyData().then(setCurrencies);
+    setTimeout(() => {
+      currencyData().then(setCurrencies);
+      setLoadingCurrencies(false);
+    }, 4000);
   }, []);
 
   return (
-    <div className="md:w-full flex flex-col gap-20 md:gap-20 lg:gap-12 py-4 md:py-10 px-4 2xl:px-44">
-      {currencies !== "" && (
-        <div className="w-full flex flex-col lg:flex-row justify-center items-center lg:items-start pt-10">
-          <Converter currencies={currencies} />
-          <Dashboard currencies={currencies} />
+    <>
+      {loadingCurrencies ? (
+        <LoadingCurrencies />
+      ) : (
+        <div className="md:w-full flex flex-col gap-20 md:gap-20 lg:gap-12 py-4 md:py-10 px-4 2xl:px-44">
+          <div className="w-full flex flex-col lg:flex-row justify-center items-center lg:items-start pt-10">
+            <Converter currencies={currencies} />
+            <Dashboard currencies={currencies} />
+          </div>
+          <div className="w-full flex flex-col justify-center items-center gap-16">
+            <CurrencyChart />
+            <TopFive />
+          </div>
         </div>
       )}
-      {currencies !== "" && (
-        <div className="w-full flex flex-col justify-center items-center gap-16">
-          <CurrencyChart />
-          <TopFive />
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
---
+++ components/LoadingCurrencies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoadingCurrencies = () => {
+  return (
+    <div className="loading-animation border-x border-teal-400">
+      <p className="w-full h-full py-8 md:px-0 underline underline-offset-2 decoration-1 decoration-dotted text-center text-2xl text-[#A3C6FF]">
+        Loading Currencies
+      </p>
+    </div>
+  );
+}
+
+export default LoadingCurrencies
