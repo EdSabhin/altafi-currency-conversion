20c65d75f66555582af53becfde2b27b14070b4c
EdSabhin <edsabhin@yahoo.com>
2024-04-25 09:06:57 -0500

    currencies validation in Homepage.

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -6,7 +6,6 @@ import Converter from "./Converter";
 import Dashboard from "./Dashboard";
 import CurrencyChart from "./CurrencyChart";
 import TopFive from "./TopFive";
-import Image from "next/image";
 
 const Homepage = () => {
   const [currencies, setCurrencies] = useState("");
@@ -17,14 +16,18 @@ const Homepage = () => {
 
   return (
     <div className="md:w-full flex flex-col gap-20 md:gap-20 lg:gap-12 py-4 md:py-10 px-4 2xl:px-44">
-      <div className="w-full flex flex-col lg:flex-row justify-center items-center lg:items-start pt-10">
-        <Converter currencies={currencies} />
-        <Dashboard currencies={currencies} />
-      </div>
-      <div className="w-full flex flex-col justify-center items-center gap-16">
-        <CurrencyChart />
-        <TopFive />
-      </div>
+      {currencies !== "" && (
+        <div className="w-full flex flex-col lg:flex-row justify-center items-center lg:items-start pt-10">
+          <Converter currencies={currencies} />
+          <Dashboard currencies={currencies} />
+        </div>
+      )}
+      {currencies !== "" && (
+        <div className="w-full flex flex-col justify-center items-center gap-16">
+          <CurrencyChart />
+          <TopFive />
+        </div>
+      )}
     </div>
   );
 };
